bfaf89db2e519dc6005a1d998f4ca32d37a554ee
Carl Bolstad <cbolstad@gmail.com>
2017-01-02 21:51:13 -0800

    enhance shopping: - add items to cart - checkout button sums items in cart before creating paymentRequest - add shipping options and shipping changed handler

--- public/js/payments.js
+++ public/js/payments.js
@@ -1,15 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+
 	
-	if (window.PaymentRequest) {
-		console.log("PaymentRequest supported!");
+if (window.PaymentRequest) {
+  console.log("PaymentRequest supported!");
+} else {
+  console.log("PaymentRequest not supported");
+}
+
+var cart = {};
+var total = 0.0;
+
+function addToCart(product) {
+	if(cart[product.id]) {
+		cart[product.id].quantity += 1;
 	} else {
-	  console.log("PaymentRequest not supported");
+		product.quantity = 1;
+		cart[product.id] = product;
 	}
-});
+	total += product.price;
+}
 
-function buy(product) {
-	//alert("buy id="+productId);
+function getDisplayItems() {
+	var items = [];
+	Object.keys(cart).forEach(function(key){
+		var product = cart[key];
+		items.push({
+			label:  product.title + " quantity: " + product.quantity,
+			amount: {currency: "USD", value: product.price * product.quantity}
+		});
+	})
+	return items;
+}
 
+function checkOut() {
 	let methodData = [
 		  {
 		    supportedMethods: ["visa", "mastercard"]
@@ -17,21 +39,24 @@ function buy(product) {
 		];
 
 	let details = {
-	  displayItems: [
-	    {
-	      label: "Original donation amount",
-	      amount: { currency: "USD", value : "65.00" }, // US$65.00
-	    },
-	    {
-	      label: "Friends and family discount",
-	      amount: { currency: "USD", value : product.price }, // -US$10.00
-	      pending: true // The price is not determined yet
-	    }
-	  ],
+	  displayItems: getDisplayItems(),
 	  total:  {
 	    label: "Total",
-	    amount: { currency: "USD", value : "55.00" }, // US$55.00
+	    amount: { currency: "USD", value : total }, 
+	  },
+	  shippingOptions: [
+	  {
+	  	id: 'standard',
+	  	label: 'Standard shipping',
+	  	amount: {currency: 'USD', value: 0},
+	  	selected: true
+	  },
+	  {
+	  	id: 'express',
+	  	label: 'Express shipping',
+	  	amount: {currency: 'USD', value: 12.00}
 	  }
+	  ]
 	};
 
 	let options = {
@@ -45,6 +70,26 @@ function buy(product) {
 		options     // optional parameter for things like shipping, etc.
 	);
 
+	request.addEventListener('shippingoptionchange', e => {
+		e.updateWith(((details, shippingOption) => {
+			var selectedShippingOption;
+			var otherShippingOption;
+			if (shippingOption === 'standard') {
+			  selectedShippingOption = details.shippingOptions[0];
+			  otherShippingOption = details.shippingOptions[1];
+			} else {
+			  selectedShippingOption = details.shippingOptions[1];
+			  otherShippingOption = details.shippingOptions[0];
+			}
+			details.total.amount.value = total + selectedShippingOption.amount.value;
+			details.displayItems.push(selectedShippingOption);
+			
+			selectedShippingOption.selected = true;
+			otherShippingOption.selected = false;
+			return Promise.resolve(details);
+		})(details, request.shippingOption));
+	});
+
 	request.show().then(function(paymentResponse) {
 	  // Process paymentResponse here
 	  paymentResponse.complete("success");
@@ -52,6 +97,5 @@ function buy(product) {
 	  console.error("Uh oh, something bad happened", err.message);
 	});
 		
-
 }
 
